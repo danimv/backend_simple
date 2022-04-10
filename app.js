a51b4c8dccc75ca4588075725ec8ac53407fc59d
Dani <danimv87@gmail.com>
2022-04-10 17:08:36 +0200

    coeficientT

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-//const bodyParser = require('body-parser'); // No longer Required
-//const mysql = require('mysql'); // Not required -> moved to userController
 
 require('dotenv').config();
 
@@ -24,14 +22,6 @@ app.use(express.static('public'));
 const handlebars = exphbs.create({ extname: '.hbs', });
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
-
-// You don't need the connection here as we have it in userController
-// let connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// });
 
 const routes = require('./server/routes/user');
 app.use('/', routes);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,17 +8,25 @@ let conn = new sqlite3.Database('server/controllers/comunitat.db', sqlite3.OPEN_
 
 // View Users
 exports.view = (req, res) => {
-  // User the connection
-  conn.all('SELECT * FROM usuari', (err, rows) => {
-  // connection.query('SELECT * FROM user WHERE status = "active"', (err, rows) => {
-    // When done with the connection, release it
-    if (!err) {
-      let removedUser = req.query.removed;
-      res.render('home', { rows, removedUser });
+      let alert2 = false;
+      // Calcul coeficient total
+      var coeficientTotal = calculaCoeficient();
+      console.log( coeficientTotal);
+      if(coeficientTotal>1 || coeficientTotal<1) {
+        alert2 = true;
+      } else{
+        alert2 = false;
+      } 
+  // Sqlite connection 
+  conn.all('SELECT * FROM usuari', (err, rows) => {    
+     // When done with the connection, release it
+    if (!err) {      
+      let alert = req.query.removed;         
+      res.render('home', {rows, alert, alert2, coeficientTotal});
     } else {
       console.log(err);
     }
-    console.log('Les dades de la base de dades: \n', rows);
+    //console.log('fsd \n', rows);
   });
 }
 
@@ -26,13 +34,13 @@ exports.view = (req, res) => {
 exports.find = (req, res) => {
   let searchTerm = req.body.search;
   // User the connection
-  connection.query('SELECT * FROM usuari WHERE nom LIKE ? OR cognom LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
+  conn.all('SELECT * FROM usuari WHERE nom LIKE ? OR cognoms LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
     if (!err) {
       res.render('home', { rows });
     } else {
       console.log(err);
     }
-    console.log('Les dades de la búsquedas: \n', rows);
+    console.log('Les dades de la búsqueda');
   });
 }
 
@@ -44,54 +52,69 @@ exports.form = (req, res) => {
 exports.create = (req, res) => {
   const { nom, cognoms, email, telefon, coeficient, estat, comentaris} = req.body;
   let searchTerm = req.body.search;
+  if(coeficient>1 || coeficient<0 || coeficient ==''){
+    //res.render('add-user', { alert2: 'Error. Indica un coeficient entre 0 i 1' });
+  }else{
   // User the connection
-  // conn.all('INSERT INTO usuari SET nom = ?, cognoms = ?, email = ?, telefon = ?, coeficient = ?, estat = ?, comentaris = ?', [nom, cognoms, email, telefon, coeficient, estat, comentaris], (err, rows) => {
-    conn.all('INSERT INTO usuari(nom, cognoms, email, telefon, coeficient, estat, comentaris) VALUES (?,?,?,?,?,?,?)', [nom, cognoms, email, telefon, coeficient, estat, comentaris], (err, rows) => {  
+    const data = new Date();  
+    const year = data.getFullYear() * 100000000; 
+    const month = (data.getMonth() + 1) * 1000000;
+    const day = data.getDate() * 10000; 
+    const hour = data.getHours() * 100;
+    const min = data.getMinutes();
+    const data2 = year + month + day + hour + min + ''
+    conn.all('INSERT INTO usuari(nom, cognoms, email, telefon, coeficient, estat, comentaris, dataAlta, dataActualitzacio) VALUES (?,?,?,?,?,?,?,?,?)', [nom, cognoms, email, telefon, coeficient, estat, comentaris, data2, data2], (err, rows) => {  
     if (!err) {
       res.render('add-user', { alert: 'Usuari afegit correctament.' });
     } else {
       console.log(err);
     }
-    console.log('Editant l`usuari: \n', rows);
-  });}
-
-
+    console.log('Editant l`usuari');
+  });
+}
+}
+  
 // Edit user
 exports.edit = (req, res) => {
   // User the connection
-  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+  conn.all('SELECT * FROM usuari WHERE id = ?', [req.params.id], (err, rows) => {
     if (!err) {
       res.render('edit-user', { rows });
     } else {
       console.log(err);
     }
-    console.log('The data from user table: \n', rows);
+    console.log('Les dades de l`usuari');
   });
 }
 
-
 // Update User
 exports.update = (req, res) => {
-  const { first_name, last_name, email, phone, comments } = req.body;
+  const { nom, cognoms, email, telefon, coeficient, estat, comentaris } = req.body;
+  const data = new Date();  
+    const year = data.getFullYear() * 100000000; 
+    const month = (data.getMonth() + 1) * 1000000;
+    const day = data.getDate() * 10000; 
+    const hour = data.getHours() * 100;
+    const min = data.getMinutes();
+    const data2 = year + month + day + hour + min + ''
   // User the connection
-  connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ? WHERE id = ?', [first_name, last_name, email, phone, comments, req.params.id], (err, rows) => {
+  conn.all('UPDATE usuari SET nom = ?, cognoms = ?, email = ?, telefon = ?, coeficient = ?, estat = ?, comentaris = ?, dataActualitzacio = ? WHERE id = ?', [nom, cognoms, email, telefon, coeficient, estat, comentaris, data2, req.params.id], (err, rows) => {
 
     if (!err) {
       // User the connection
-      connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
-        // When done with the connection, release it
-        
+      conn.all('SELECT * FROM usuari WHERE id = ?', [req.params.id], (err, rows) => {
+        // When done with the connection, release it        
         if (!err) {
-          res.render('edit-user', { rows, alert: `${first_name} has been updated.` });
+          res.render('edit-user', { rows, alert: `Les dades de ${nom} s'han actualitzat.` });
         } else {
           console.log(err);
         }
-        console.log('The data from user table: \n', rows);
+        console.log('Actualitzant usuari');
       });
     } else {
       console.log(err);
     }
-    console.log('The data from user table: \n', rows);
+    console.log('Actualitzant usuari');
   });
 }
 
@@ -114,14 +137,14 @@ exports.delete = (req, res) => {
 
   // Hide a record
 
-  connection.query('UPDATE user SET status = ? WHERE id = ?', ['removed', req.params.id], (err, rows) => {
+  conn.all('UPDATE usuari SET estat = ? WHERE id = ?', ['Baixa', req.params.id], (err, rows) => {
     if (!err) {
-      let removedUser = encodeURIComponent('User successeflly removed.');
+      let removedUser = encodeURIComponent('Usuari donat de baixa.');
       res.redirect('/?removed=' + removedUser);
     } else {
       console.log(err);
     }
-    console.log('The data from beer table are: \n', rows);
+    console.log('L`usuari es dona de baixa');
   });
 
 }
@@ -130,13 +153,27 @@ exports.delete = (req, res) => {
 exports.viewall = (req, res) => {
 
   // User the connection
-  connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+  conn.all('SELECT * FROM usuari WHERE id = ?', [req.params.id], (err, rows) => {
     if (!err) {
       res.render('view-user', { rows });
     } else {
       console.log(err);
     }
-    console.log('The data from user table: \n', rows);
+    console.log('Dades de l`usuari');
   });
+}
 
+function calculaCoeficient(){
+  coeficientT = 0;
+  conn.all('SELECT * FROM usuari', (err, rows, cT) =>  {          
+        
+     // Calcul coeficient total
+     rows.forEach(row => {
+       cT = cT + row.coeficient;        
+     });       
+     cT = cT.toFixed(5);
+      
+     return cT
+    });   
+    return coeficientT 
 }
