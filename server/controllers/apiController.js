57b17b2743277a886877ababdb9f65892da69f66
danimv <danimv87@gmail.com>
2022-11-05 23:18:37 +0100

    simple init

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -14,7 +14,7 @@ localStorage = new LocalStorage('./scratch');
 // Vinculació de comunitat amb servidor extern
 exports.init = (req, res) => {
   var { idComunitat, nomComunitat, comentaris } = req.body;
-  //token = req.headers.authorization;
+  token = req.headers.authorization;
   data = exportedC.calcularData();
   message = "";
   tipus = "Dades comunitat";
@@ -23,33 +23,35 @@ exports.init = (req, res) => {
   // Sqlite connexió 
   // exportedC.checkFileExists(location, function check(error) {
   // if (!error) {
-  conn.all('SELECT * FROM comunitat ORDER BY id DESC LIMIT 1', (err, rows) => {
-    if (!err) {
-      if (rows.length == 0 || rows[0].mode == 0) {
-        if (nomComunitat && idComunitat) {
-          nomComunitat = nomComunitat.toUpperCase();
-          conn.all('INSERT INTO comunitat(idComunitat, nomComunitat, comentaris, sync,mode) VALUES (?,?,?,?,?)', [idComunitat, nomComunitat, comentaris, 1, 0], (err, rows) => {
-            if (!err) {
-              message = 'Comunitat vinculada';
-              httpResponse(req, res, 200, 'OK', message);
-            } else {
-              message = 'Comunitat no vinculada. Error de base de dades: ' + err;
-              httpResponse(req, res, 400, 'KO', message);
-            }
-          });
-        } else {
-          message = 'Comunitat no vinculada. Falta idComunitat o nomComunitat';
-          httpResponse(req, res, 400, 'KO', message);
-        }
-      } else {
-        message = 'Mode Offline, no és possible actualitzar dades ';
-        httpResponse(req, res, 400, 'KO', message);
-      }
-    } else {
-      message = 'Comunitat no vinculada. Error de base de dades: ' + err;
-      httpResponse(req, res, 400, 'KO', message);
-    }
-  });
+  // conn.all('SELECT * FROM comunitat ORDER BY id DESC LIMIT 1', (err, rows) => {
+  //   if (!err) {
+  //     if (rows.length == 0 || rows[0].mode == 0) {
+  //       if (nomComunitat && idComunitat) {
+  //         nomComunitat = nomComunitat.toUpperCase();
+  //         conn.all('INSERT INTO comunitat(idComunitat, nomComunitat, comentaris, sync,mode) VALUES (?,?,?,?,?)', [idComunitat, nomComunitat, comentaris, 1, 0], (err, rows) => {
+  //           if (!err) {
+  //             message = 'Comunitat vinculada';
+  //             httpResponse(req, res, 200, 'OK', message, insertApiTable);
+  //           } else {
+  //             message = 'Comunitat no vinculada. Error de base de dades: ' + err;
+  //             httpResponse(req, res, 400, 'KO', message, insertApiTable);
+  //           }
+  //         });
+  //       } else {
+  //         message = 'Comunitat no vinculada. Falta idComunitat o nomComunitat';
+  //         httpResponse(req, res, 400, 'KO', message, insertApiTable);
+  //       }
+  //     } else {
+  //       message = 'Mode Offline, no és possible actualitzar dades ';
+  //       httpResponse(req, res, 400, 'KO', message, insertApiTable);
+  //     }
+  //   } else {
+  //     message = 'Comunitat no vinculada. Error de base de dades: ' + err;
+  //     httpResponse(req, res, 400, 'KO', message, insertApiTable);
+  //   }
+  // });
+  message = 'Test';
+      httpResponse(req, res, 400, 'KO', message, insertApiTable);
 }
 // {"idComunitat":"1",
 // "nomComunitat":"Cornella del Terri"}
@@ -198,7 +200,7 @@ function updateCoeficientsTable(data) {
 }
 
 // Resposta http
-function httpResponse(req, res, code, strResult, msg) {
+function httpResponse(req, res, code, strResult, msg, callback) {
   res.statusCode = code;
   res.setHeader('Content-Type', 'application/json');
   const body = {
