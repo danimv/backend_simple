36912aa89f67e2dd45e75b6cf4da52a31b16c4c0
Dani <danimv87@gmail.com>
2023-10-28 16:26:50 +0200

    backend_simple

--- server/controllers/iniciController.js
+++
@@ -1,5 +0,0 @@
-// Vista inici
-exports.view = (req, res) => {
-  let alert2 = false;
-  res.render('inici');  
-}
--- server/routes/inici.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const rutesInici = express.Router();
-const iniciController = require('../controllers/iniciController');
-
-// Routes
-rutesInici.get('/', iniciController.view);
- 
-module.exports = rutesInici;
